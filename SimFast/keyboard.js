8ad6960c5a5f4e2935ba8b243a4883e04210ffaa
tinkhuu <84937325+tinkhuu@users.noreply.github.com>
2023-04-21 02:54:29 -0400

    Created Keyboard Functions,

--- SimFast/keyboard.js
+++ SimFast/keyboard.js
@@ -2,3 +2,13 @@ function addCharacter(character) {
     const textField = document.getElementById("keyboardtextfield");
     textField.value += character;
   }
+
+  function deleteCharacter() {
+    const textField = document.getElementById("keyboardtextfield");
+    textField.value = textField.value.slice(0, -1);
+  }
+
+  function displayflightstrip(){
+  const textField = document.getElementById("callsigntext");
+  textField.value += "001";
+  }
