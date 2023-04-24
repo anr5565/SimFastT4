b5fb69218a0aca86445ba6e13276e346ead13ade
tinkhuu <84937325+tinkhuu@users.noreply.github.com>
2023-04-23 22:21:44 -0400

    finishedFlightStrip

--- SimFast/keyboard.js
+++ SimFast/keyboard.js
@@ -58,58 +58,320 @@ function addCharacter(character) {
     const ad2 = document.getElementById("ad");
     const time2 = document.getElementById("time");
     const etg2 = document.getElementById("etg");
+
+
+    textField2.value = "";
+    ticketid2.value = "";
+    type2.value = "";
+    fixpair2.value = "";
+    sp12.value = "";
+    sp22.value = "";
+    pos2.value = "";
+    ad2.value = "";
+    time2.value = "";
+    etg2.value = "";
   
-    textField2.value += "DAL5689";
+    textField2.value += "N8502F";
     ticketid2.value += "002";
-    type2.value += "A320";
-    fixpair2.value += "ORD.PHL";
-    sp12.value += "PHL";
+    type2.value += "C421";
+    fixpair2.value += "RBV.VCN";
+    sp12.value += "VCN";
     sp22.value += "";
     pos2.value += "1V";
-    ad2.value += "A";
-    time2.value += "00:02:32";
+    ad2.value += "E";
+    time2.value += "00:07:15";
     etg2.value += "A090H195";
 
   }
 //
 
   function displayflightstrip3(){
-    
+  const textField = document.getElementById("callsigntext");
+  const ticketid = document.getElementById("ticketid");
+  const type = document.getElementById("type");
+  const fixpair = document.getElementById("fixpair");
+  const sp1 = document.getElementById("sp1");
+  const sp2 = document.getElementById("sp2");
+  const pos = document.getElementById("pos");
+  const ad = document.getElementById("ad");
+  const time = document.getElementById("time");
+  const etg= document.getElementById("etg");
+
+  textField.value = "";
+  ticketid.value = "";
+  type.value = "";
+  fixpair.value = "";
+  sp1.value = "";
+  sp2.value = "";
+  pos.value = "";
+  ad.value = "";
+  time.value = "";
+  etg.value = "";
+
+  textField.value += "DAL609";
+  ticketid.value += "009";
+  type.value += "B350";
+  fixpair.value += "CYN.PHL";
+  sp1.value += "PHL";
+  sp2.value += "";
+  pos.value += "1V";
+  ad.value += "A";
+  time.value += "00:05:36";
+  etg.value += "A090H195";
   }
 //
 
   function displayflightstrip4(){
+
+  const textField = document.getElementById("callsigntext");
+  const ticketid = document.getElementById("ticketid");
+  const type = document.getElementById("type");
+  const fixpair = document.getElementById("fixpair");
+  const sp1 = document.getElementById("sp1");
+  const sp2 = document.getElementById("sp2");
+  const pos = document.getElementById("pos");
+  const ad = document.getElementById("ad");
+  const time = document.getElementById("time");
+  const etg= document.getElementById("etg");
+
+  textField.value = "";
+  ticketid.value = "";
+  type.value = "";
+  fixpair.value = "";
+  sp1.value = "";
+  sp2.value = "";
+  pos.value = "";
+  ad.value = "";
+  time.value = "";
+  etg.value = "";
+
+  textField.value += "AAL2020";
+  ticketid.value += "020";
+  type.value += "E190";
+  fixpair.value += "CYN.PHL";
+  sp1.value += "PHL";
+  sp2.value += "";
+  pos.value += "1V";
+  ad.value += "A";
+  time.value += "00:10:02";
+  etg.value += "A090H195";
     
   }
 //
 
   function displayflightstrip5(){
-    
+    const textField = document.getElementById("callsigntext");
+  const ticketid = document.getElementById("ticketid");
+  const type = document.getElementById("type");
+  const fixpair = document.getElementById("fixpair");
+  const sp1 = document.getElementById("sp1");
+  const sp2 = document.getElementById("sp2");
+  const pos = document.getElementById("pos");
+  const ad = document.getElementById("ad");
+  const time = document.getElementById("time");
+  const etg= document.getElementById("etg");
+
+  textField.value = "";
+  ticketid.value = "";
+  type.value = "";
+  fixpair.value = "";
+  sp1.value = "";
+  sp2.value = "";
+  pos.value = "";
+  ad.value = "";
+  time.value = "";
+  etg.value = "";
+
+  textField.value += "SWA1126";
+  ticketid.value += "026";
+  type.value += "B738";
+  fixpair.value += "PHL.OOD";
+  sp1.value += "OOD";
+  sp2.value += "";
+  pos.value += "1V";
+  ad.value += "D";
+  time.value += "00:11:13";
+  etg.value += "";
   }
 
   //
   function displayflightstrip6(){
-    
+    const textField = document.getElementById("callsigntext");
+  const ticketid = document.getElementById("ticketid");
+  const type = document.getElementById("type");
+  const fixpair = document.getElementById("fixpair");
+  const sp1 = document.getElementById("sp1");
+  const sp2 = document.getElementById("sp2");
+  const pos = document.getElementById("pos");
+  const ad = document.getElementById("ad");
+  const time = document.getElementById("time");
+  const etg= document.getElementById("etg");
+
+  textField.value = "";
+  ticketid.value = "";
+  type.value = "";
+  fixpair.value = "";
+  sp1.value = "";
+  sp2.value = "";
+  pos.value = "";
+  ad.value = "";
+  time.value = "";
+  etg.value = "";
+
+  textField.value += "N71453";
+  ticketid.value += "053";
+  type.value += "BE20";
+  fixpair.value += "RBV.VCN";
+  sp1.value += "OVCN";
+  sp2.value += "";
+  pos.value += "1V";
+  ad.value += "D";
+  time.value += "00:06:40";
+  etg.value += "A090H195";
   }
 //
 
   function displayflightstrip7(){
-    
+      const textField = document.getElementById("callsigntext");
+    const ticketid = document.getElementById("ticketid");
+    const type = document.getElementById("type");
+    const fixpair = document.getElementById("fixpair");
+    const sp1 = document.getElementById("sp1");
+    const sp2 = document.getElementById("sp2");
+    const pos = document.getElementById("pos");
+    const ad = document.getElementById("ad");
+    const time = document.getElementById("time");
+    const etg= document.getElementById("etg");
+  
+    textField.value = "";
+    ticketid.value = "";
+    type.value = "";
+    fixpair.value = "";
+    sp1.value = "";
+    sp2.value = "";
+    pos.value = "";
+    ad.value = "";
+    time.value = "";
+    etg.value = "";
+  
+    textField.value += "AAL456";
+    ticketid.value += "056";
+    type.value += "BE20";
+    fixpair.value += "PHL.ACY";
+    sp1.value += "ACY";
+    sp2.value += "";
+    pos.value += "1V";
+    ad.value += "D";
+    time.value += "00:09:17";
+    etg.value += "";
   }
 //
 
   function displayflightstrip8(){
-    
+    const textField = document.getElementById("callsigntext");
+    const ticketid = document.getElementById("ticketid");
+    const type = document.getElementById("type");
+    const fixpair = document.getElementById("fixpair");
+    const sp1 = document.getElementById("sp1");
+    const sp2 = document.getElementById("sp2");
+    const pos = document.getElementById("pos");
+    const ad = document.getElementById("ad");
+    const time = document.getElementById("time");
+    const etg= document.getElementById("etg");
+  
+    textField.value = "";
+    ticketid.value = "";
+    type.value = "";
+    fixpair.value = "";
+    sp1.value = "";
+    sp2.value = "";
+    pos.value = "";
+    ad.value = "";
+    time.value = "";
+    etg.value = "";
+  
+    textField.value += "DAL3363";
+    ticketid.value += "063";
+    type.value += "B752";
+    fixpair.value += "PHL.EWR";
+    sp1.value += "EWR";
+    sp2.value += "";
+    pos.value += "1V";
+    ad.value += "D";
+    time.value += "00:04:09";
+    etg.value += "";
   }
 //
 
   function displayflightstrip9(){
-    
+    const textField = document.getElementById("callsigntext");
+    const ticketid = document.getElementById("ticketid");
+    const type = document.getElementById("type");
+    const fixpair = document.getElementById("fixpair");
+    const sp1 = document.getElementById("sp1");
+    const sp2 = document.getElementById("sp2");
+    const pos = document.getElementById("pos");
+    const ad = document.getElementById("ad");
+    const time = document.getElementById("time");
+    const etg= document.getElementById("etg");
+  
+    textField.value = "";
+    ticketid.value = "";
+    type.value = "";
+    fixpair.value = "";
+    sp1.value = "";
+    sp2.value = "";
+    pos.value = "";
+    ad.value = "";
+    time.value = "";
+    etg.value = "";
+  
+    textField.value += "TWY85";
+    ticketid.value += "085";
+    type.value += "C25B";
+    fixpair.value += "CYN.PHL";
+    sp1.value += "PHL";
+    sp2.value += "";
+    pos.value += "1V";
+    ad.value += "A";
+    time.value += "00:03:33";
+    etg.value += "";
   }
 //
 
   function displayflightstrip10(){
-    
+    const textField = document.getElementById("callsigntext");
+    const ticketid = document.getElementById("ticketid");
+    const type = document.getElementById("type");
+    const fixpair = document.getElementById("fixpair");
+    const sp1 = document.getElementById("sp1");
+    const sp2 = document.getElementById("sp2");
+    const pos = document.getElementById("pos");
+    const ad = document.getElementById("ad");
+    const time = document.getElementById("time");
+    const etg= document.getElementById("etg");
+  
+    textField.value = "";
+    ticketid.value = "";
+    type.value = "";
+    fixpair.value = "";
+    sp1.value = "";
+    sp2.value = "";
+    pos.value = "";
+    ad.value = "";
+    time.value = "";
+    etg.value = "";
+  
+    textField.value += "UPS2196";
+    ticketid.value += "096";
+    type.value += "C25B";
+    fixpair.value += "PHL.OOD";
+    sp1.value += "OOD";
+    sp2.value += "";
+    pos.value += "1V";
+    ad.value += "D";
+    time.value += "00:06:18";
+    etg.value += "";
   }
 
 //
